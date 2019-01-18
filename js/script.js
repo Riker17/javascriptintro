5a43f3ab93aa3cded9853e9a8eef683dcb4d42f9
Will Robertson <will@wpasradio.com>
2019-01-17 19:22:07 -0800

    Worked on script and additional practice in index page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-var obj = {
-  firstName: "Will",
-  lastName: "Robertson",
-  age: 39
+const me = {
+  name: 'Will',
+  age: 39,
+  eyes: brown,
+  height: 70,
 };
+
+document.write(me.name + me.age + me.eyes + me.height);
+
+const imgs = document.getElementsByTagName('img');
+console.log('img', imgs[0]);
+const px = 'px';
+let move = 0;
+
+
+
+setInterval( () => {
+  move += 20;
+  img.style.right = move + px;
+}, 100)
